94e01a0f0e832fd3d1dc19c455592fdc573451e5
erlie <lilimin@social-touch.com>
2019-07-21 16:09:22 +0800

    增加了打印容器内bean的代码

--- src/main/java/com/st/part1/Main.java
+++ src/main/java/com/st/part1/Main.java
@@ -7,6 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        String names[] = context.getBeanDefinitionNames();
+        for (int i = 0; i < names.length; i++) {
+            // knight
+            // weapon
+            System.out.println(names[i]);
+        }
         BraveKnight knight = context.getBean(BraveKnight.class);
         // knife
         System.out.println(knight.getWeapon().getType());
--- src/main/java/com/st/part2/Main.java
+++ src/main/java/com/st/part2/Main.java
@@ -6,11 +6,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(MyConfig.class);
-        context.refresh();
-//        BraveKnight knight = context.getBean(BraveKnight.class);
-        BraveKnight knight = SpringHelper.getBean(BraveKnight.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+        String names[] = context.getBeanDefinitionNames();
+        for (int i = 0; i < names.length; i++) {
+            // myConfig
+            // braveKnight
+            // weapon
+            System.out.println(names[i]);
+        }
+        BraveKnight knight = context.getBean(BraveKnight.class);
+//        BraveKnight knight = SpringHelper.getBean(BraveKnight.class);
         // knife
         System.out.println(knight.getWeapon().getClassC().getName());
         context.close();
