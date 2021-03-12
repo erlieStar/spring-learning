f2956c59752dc7f91424c4303679a804471d81b6
lilimin <290059742@qq.com>
2021-03-12 12:28:22 +0800

    先暂存一下正确的

---
+++ configuration-metadata/src/main/java/com/javashitang/User.java
@@ -0,0 +1,17 @@
+package com.javashitang;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author lilimin
+ * @since 2021-03-12
+ */
+@Data
+@ToString
+public class User {
+
+    private Long id;
+    private String name;
+
+}
---
+++ configuration-metadata/src/test/java/com/javashitang/ExtensibleXmlAuthoringDemoTest.java
@@ -0,0 +1,20 @@
+package com.javashitang;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ExtensibleXmlAuthoringDemo.
+ */
+public class ExtensibleXmlAuthoringDemoTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
