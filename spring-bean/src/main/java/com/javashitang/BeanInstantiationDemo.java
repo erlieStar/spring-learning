5d7a2155106d621ffdc7060e6c6b7533b6a8bb15
lilimin <290059742@qq.com>
2021-05-16 21:33:54 +0800

    增加部分注释

--- spring-bean/src/main/java/com/javashitang/BeanInstantiationDemo.java
+++ spring-bean/src/main/java/com/javashitang/BeanInstantiationDemo.java
@@ -1,6 +1,7 @@
 package com.javashitang;
 
 import com.javashitang.domain.User;
+import com.javashitang.factory.UserFactoryBean;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,11 +13,13 @@ public class BeanInstantiationDemo {
 		User user2 = beanFactory.getBean("user-by-static-method", User.class);
 		User user3 = beanFactory.getBean("user-by-factory", User.class);
 		User user4 = beanFactory.getBean("user-by-factory-bean", User.class);
+        UserFactoryBean userFactoryBean = beanFactory.getBean("&" + "user-by-factory-bean", UserFactoryBean.class);
 
 		System.out.println(user1);
 		System.out.println(user2);
 		System.out.println(user3);
 		System.out.println(user4);
+        System.out.println(userFactoryBean);
 
 	}
 }
